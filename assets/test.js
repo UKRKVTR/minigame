2a470eb77507c9094979085db759840c832e6acd
Danylo <warfais6@gmail.com>
2023-09-05 22:01:05 +0300

    Add Game3

--- assets/test.js
+++ assets/test.js
@@ -5,9 +5,13 @@ function randomInteger(min, max) {
 
 let gameEnd;
 
-let gameChoise = +prompt("Выберете игру: 1 - умножение, 2 - угадай число");
+let gameChoise = +prompt(
+  "Выберете игру: 1 - умножение, 2 - угадай число, 3 - угадай слово"
+);
 
-function game(n1, n2) {
+function game() {
+  let n1 = randomInteger(1, 10);
+  let n2 = randomInteger(1, 10);
   let answer = +prompt(`Сколько будет ${n1} умножить на ${n2}`);
   if ((res = n1 * n2 === answer)) {
     alert("Правильно");
@@ -20,8 +24,9 @@ function game(n1, n2) {
 }
 
 function game2() {
-  let x1 = Math.trunc(Math.random() * 11);
+  let x1 = randomInteger(1, 10);
   let answ = +prompt(`я загадал число между ${x1 - 3} и ${x1 + 3}`);
+
   if (answ < x1 - 3 || answ > x1 + 3) {
     throw new RangeError("Вы не попали даже в диапозон");
   } else if (typeof answ !== "number") {
@@ -34,41 +39,42 @@ function game2() {
   }
 }
 
-function menu(gameChoise) {
-  if (gameChoise === 1) {
-    for (let i = 0; i < 3 && !gameEnd; ++i) {
-      game((n1 = randomInteger(1, 10)), (n2 = randomInteger(1, 10)));
-    }
-  } else if (gameChoise === 2) {
-    // for (let i = 0; i < 3 && !gameEnd; i++) {
-    //   game3();
-    // }
-    game3();
-  } else {
-    alert("Данная игра в розработке");
-  }
-}
-
 function game3() {
   const words = ["Кот", "Дисперсия", "Олег"];
   let randomIndex = randomInteger(0, 2);
-  let lives = 6;
   const guess = words[randomIndex];
-  let underscores = "_ ".repeat(guess.length);
   const firstLatters = guess[0];
+  let underscores = "_ ".repeat(guess.length);
   let userAnsw = prompt(`Guess a word: ${underscores}`);
-  for (let i = 0; i < 6 && !gameEnd; i++) {
+
+  for (let lives = 6; lives < 0 && !gameEnd; lives--) {
     if (userAnsw === guess) {
       alert(`Правильно это слово ${guess} `);
       gameEnd = true;
+    } else if (i === 6 && !gameEnd) {
+      alert(`Вы проиграли( \n слово было ${guess}`);
     } else {
       userAnsw = prompt(
         `Guess a word: ${underscores} \n у вас осталось ${lives} \n первая буква ${firstLatters}`
       );
-      console.log(guess);
-      lives--;
     }
+    console.log(guess);
   }
-  alert(`Вы проиграли( \n слово было ${guess}`);
+}
+
+function menu(gameChoise) {
+  if (gameChoise === 1) {
+    for (let i = 0; i < 3 && !gameEnd; ++i) {
+      game();
+    }
+  } else if (gameChoise === 2) {
+    for (let i = 0; i < 3 && !gameEnd; i++) {
+      game2();
+    }
+  } else if (gameChoise === 3) {
+    game3();
+  } else {
+    alert("Данная игра в розработке");
+  }
 }
 menu(gameChoise);
